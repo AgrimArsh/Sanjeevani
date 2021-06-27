bbc605a9d12952805295ffbedd361bafa0a39a9b
Agrim Arsh <68107259+AgrimArsh@users.noreply.github.com>
2021-06-27 21:03:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Admin from './components/routes/admin';
+import User from './components/routes/user';
+import SplashScreen from './components/splashScreen';
+
+export default function App() {
+    return(
+    <SplashScreen />
+    )
+};
+
+
+
+
